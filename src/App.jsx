fbc0651d6be77aceb9c15403878d9de7ffe90f98
rafrex <code@rafrex.com>
2016-09-02 13:52:38 -0700

    Create and style app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import style from './app.style.js';
 import Interactive from '../../react-interactive-component/src/index';
+import style from './app.style';
 
 function App() {
   return (
     <div style={style.root}>
-      App component
+      <h1 style={style.title}>React Interactive</h1>
+      <div style={style.repoLink}><Interactive
+        as="a"
+        href="https://github.com/rafrex/react-interactive"
+        // use the spread operator to add state styles - equivalent to commented code below
+        {...style.link}
+        // normal={style.link.normal}
+        // hover={style.link.hover}
+        // active={style.link.active}
+        // touchActive={style.link.touchActive}
+        // style={style.link.style}
+      >https://github.com/rafrex/react-interactive</Interactive></div>
+
       <Interactive as="div" hover={{ color: 'green' }}>Interactive component</Interactive>
+
+      <div style={style.creditLine}>{'Code and concept by '}
+        <Interactive
+          as="a"
+          href="http://www.rafaelpedicini.com/"
+          {...style.link}
+        >Rafael Pedicini</Interactive>
+      </div>
     </div>
   );
 }
--- src/app.style.js
+++ src/app.style.js
@@ -1,5 +1,8 @@
 export default {
   root: {
+    maxWidth: '400px',
+    padding: '10px 15px 25px',
+    margin: '0 auto',
     fontFamily: 'helvetica neue, helvetica, sans-serif',
     fontWeight: '300',
     fontSize: '16px',
@@ -8,5 +11,36 @@ export default {
     MozTextSizeAdjust: 'none',
     msTextSizeAdjust: 'none',
     textSizeAdjust: 'none',
+  },
+  title: {
+    fontSize: '24px',
+  },
+  link: {
+    style: {
+      fontSize: '14px',
+      msTouchAction: 'manipulation',
+      touchAction: 'manipulation',
+    },
+    normal: {
+      borderBottom: '1px dotted rgb(0, 168, 0)',
+      color: 'rgb(128, 128, 128)',
+    },
+    hover: {
+      borderBottom: '1px solid rgb(0, 168, 0)',
+      color: 'black',
+    },
+    active: 'hover',
+    touchActive: {
+      borderBottom: '1px dashed rgb(0, 168, 0)',
+      color: 'black',
+    },
+  },
+  repoLink: {
+    margin: '-1px 1px 15px',
+  },
+  creditLine: {
+    fontSize: '14px',
+    color: 'rgb(128, 128, 128)',
+    margin: '20px 0 0 1px',
   },
 };
