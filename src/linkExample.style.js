8c5e2f905ce22bbb0c6b4ae4caf4593eeaa92b19
rafrex <code@rafrex.com>
2016-09-24 14:30:04 -0700

    Update link example spacing

--- src/linkExample.style.js
+++ src/linkExample.style.js
@@ -1,19 +1,26 @@
 export default {
   root: {
-    margin: '10px 0',
+    marginBottom: '15px',
   },
-  title: { fontSize: '18px' },
+  title: {
+    fontSize: '18px',
+    marginBottom: '1px',
+  },
   subTitle: {
     fontSize: '15px',
     color: 'rgb(128, 128, 128)',
-    margin: '1px 0 5px',
+    margin: '0 0 5px',
   },
   link: {
     hover: { color: 'green' },
     hoverActive: { color: 'red' },
     touchActive: { color: 'blue' },
     keyActive: { color: 'orange' },
-    style: { borderBottom: '1px dotted' },
+    style: {
+      borderBottom: '1px dotted',
+      msTouchAction: 'manipulation',
+      touchAction: 'manipulation',
+    },
     focus: {
       style: {
         outline: '2px solid rgb(0, 152, 0)',
