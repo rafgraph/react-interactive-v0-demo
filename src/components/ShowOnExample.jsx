a0701f9fea6316915ef7bab0f625dc0f95831c02
rafrex <code@rafrex.com>
2016-11-21 15:49:28 -0500

    Update to use nonContainedChild api

--- src/components/ShowOnExample.jsx
+++ src/components/ShowOnExample.jsx
@@ -37,7 +37,7 @@ class ShowOnExample extends React.Component {
           as="span"
           {...s.infoButton}
           onStateChange={this.handleStateChange}
-          checkDOMChildren
+          nonContainedChild
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
